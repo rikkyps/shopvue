16c99091f5449e89033e2eb6cab268e9f2604519
rikkyps <rikky.putra14@gmail.com>
2020-10-23 15:07:15 +0700

    aplikasi front end done

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Header/>
+    <HeroCarousel/>
+    <Slider/>
+    <Instagram/>
+    <Partner/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import HeroCarousel from '@/components/HeroCarousel.vue'
+import Slider from '@/components/Slider.vue'
+import Instagram from '@/components/Instagram.vue'
+import Partner from '../components/Partner.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    Header,
+    HeroCarousel,
+    Slider,
+    Instagram,
+    Partner,
+    Footer
+  }
+}
+</script>
---
+++ src/views/Product.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="Product">
+    <Header/>
+    <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text product-more text-left">
+                        <router-link :to="{name : 'Home'}"><i class="fa fa-home"></i> Home</router-link>
+                        <span>Detail</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="product-pic-zoom">
+                                <img class="product-big-img" :src="primaryImage" alt="" />
+                            </div>
+                            <div class="product-thumbs">
+                                <carousel :dots="false" :nav="false" class="product-thumbs-track ps-slider">
+                                    <div @click.prevent="changePrimaryImage(images[0])" class="pt" :class="images[0] == primaryImage ? 'active' : ''">
+                                        <img src="img/mickey1.jpg" alt="" />
+                                    </div>
+
+                                    <div @click="changePrimaryImage(images[1])" class="pt" :class="images[1] == primaryImage ? 'active' : ''">
+                                        <img src="img/mickey2.jpg" alt="" />
+                                    </div>
+
+                                    <div @click="changePrimaryImage(images[2])" class="pt" :class="images[2] == primaryImage ? 'active' : ''">
+                                        <img src="img/mickey3.jpg" alt="" />
+                                    </div>
+
+                                    <div @click="changePrimaryImage(images[3])" class="pt" :class="images[3] == primaryImage ? 'active' : ''">
+                                        <img src="img/mickey4.jpg" alt="" />
+                                    </div>
+                                </carousel>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="product-details text-left">
+                                <div class="pd-title">
+                                    <span>oranges</span>
+                                    <h3>Pure Pineapple</h3>
+                                </div>
+                                <div class="pd-desc">
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
+                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
+                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
+                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
+                                    </p>
+                                    <h4>$495.00</h4>
+                                </div>
+                                <div class="quantity">
+                                    <router-link :to="{name: 'ShopingCart'}" class="primary-btn pd-cart">Add To Cart</router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Product Shop Section End -->
+    <Related/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Footer from '../components/Footer.vue'
+import Related from '../components/products/Related.vue'
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+  name: 'Product',
+  components: {
+    // HelloWorld
+    Header,
+    Footer,
+    carousel,
+    Related
+  },
+  data(){
+    return{
+      primaryImage: 'img/mickey1.jpg',
+      images: [
+        'img/mickey1.jpg',
+        'img/mickey2.jpg',
+        'img/mickey3.jpg',
+        'img/mickey4.jpg'
+      ]
+    }
+  },
+  methods: {
+    changePrimaryImage(image){
+      this.primaryImage = image
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .product-thumbs .pt {
+    margin-right: 10px;
+  }
+</style>
